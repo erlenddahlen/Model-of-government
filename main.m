88a7d5e28ee2f4dcc0af1ffc06d4e2654c2b076a
Erlend Dahlen <37074633+erlenddahlen@users.noreply.github.com>
2020-10-20 17:59:26 +0200

    Adding modularized version + cleaning
    The model approximates data for the Norwegian government quite well. Might need to make some adjustments with regards to BNP and private consumption. 
    
    Have added a business cycle. Cleaned and modularized the model. 
    
    In main I have cleaned up parameters and added a new plot for Consumption.

--- main.m
+++ main.m
@@ -1,40 +1,66 @@
-Tg = 0.1; %tidskonstant government
-sigma = 0.5; %offentlig pengebruk
-tx = 0.4; %skatt
-To = 0.01; %tidskonstant offentlig ansatte
-Tp = 0.01; %tidskonstant nye lån
-Tf = 0.5; %tidskonstant firmaer
+% Notes about system: 
+% Flow of money is given in MNOK
+
+% 1 Parameters
+% Time lags 
+Tg = 0.3; %Government
+Tpub = 0.9; %Public sector
+Tpriv = 0.9; %Private sector
+Tf = 0.3; %Firms
+% Regulator
+Kp = 1*10^6; %Proportional part
+Ki = 0.2*10^6; %Integral part
+Kd = 0.2*10^6; %Derivative part
+%Taxes
+tx_priv = 0.4; %Private tax
+tx_firm = 0.3; %Firm tax
 VAT = 0.07; %Value-added Tax
-S = 1; %lønnskrav 
-target = 3500000; %arbiedsutnyytelse
-Kp = 10000;
+%Other 
+sigma = 0.6; %Amount of government spending directed at firms
+target_employment = 2700000; %Ideal number of people employed
+Start = 1*10^6; %Initialize economy (1000mrd)
+Amp = 0.1*10^6; %business cycle (100mrd)
 
-sim_time = 200;
+sim_time = 100;
 
-out = sim('gov_extended_2', sim_time);
+out = sim('gov_modularised', sim_time);
 
-figure('rend','painters','pos',[10 10 750 400])
+figure('rend','painters','pos',[1 600 750 400])
 hold on;
-plot(out.offentlig, "b");
-plot(out.privat, "r");
-plot(out.total, "g");
+plot(out.e_public, "b");
+plot(out.e_private, "r");
+plot(out.e_total, "g");
 title("Employment");
-xlabel("time [s]");
-ylabel("employment");
+xlabel("time [year]");
+ylabel("Employment[People]");
 grid on;
 hold off;
-legend({"offentlig","privat","total"}, "Location", "northeast");
+legend({"public","private","total"}, "Location", "northeast");
 %legend({"Ck", "Cw"}, "Location", "northeast");
 %legend({"Loans"}, "Location", "northeast");
 
-figure('rend','painters','pos',[10 10 750 400])
+figure('rend','painters','pos',[10 100 750 400])
 hold on;
-plot(out.penger, "b");
+plot(out.regulator, "b");
 plot(out.taxes, "r");
-plot(out.spending, "g");
-title("Money");
-xlabel("time [s]");
-ylabel("Money");
+plot(out.business_cycle);
+plot(out.total_spend, "g");
+title("Government");
+xlabel("time [Year]");
+ylabel("Money[MNOK]");
 grid on;
 hold off;
-legend({"penger","taxes","spending"}, "Location", "northeast");
+legend({"money-reg","taxes","business cycle", "total spend"}, "Location", "northeast");
+
+figure('rend','painters','pos',[750 100 750 400])
+hold on;
+plot(out.c_public, "b");
+plot(out.c_private, "r");
+plot(out.BNP, "g");
+title("Consumption");
+xlabel("time [Year]");
+ylabel("Money[MNOK]");
+grid on;
+hold off;
+legend({"Goverment","Households","BNP"}, "Location", "northeast");
+
